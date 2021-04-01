875e6f353db373702e702d331c81c38e9edd5c04
DevDohz <dev@dohz.net>
2021-04-01 23:19:37 +0200

    Uniform couleur OJ + Formulaire Contact + recaptcha

--- ojsitefront/pages/contact.js
+++ ojsitefront/pages/contact.js
@@ -5,10 +5,12 @@ import s from '../styles/contact.module.css'
 export default function Contact() {
     return (
     <>
+    {/* Script ReCaptcha Google : à mettre dans le Header de préférence */}
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <div className={s.divcontact}>
       {/* Titre de la page */}
       <p className={s.title}>Nous Rencontrer</p>
-      <div class="grid grid-cols-2 divide-x-2 text-center divide-ojgreen">
+      <div class="grid sm:grid-cols-2 sm:divide-x-2 sm:divide-y-0 divide-y-2 divide-ojgreen text-center">
         {/* Partie Gauche : info de contact */}
         <div>
           <p class="textOJsoulignerVert text-2xl"><span class="font-bold">Open</span> <span class="text-ojgreen">Jujitsu</span></p>
@@ -89,30 +91,36 @@ export default function Contact() {
   
     return (
       <form onSubmit={sendContactMessage} class="space-y-4">
-        <div class="flex flex-wrap mx-10 space-y-4 md:space-y-0">
-          <label htmlFor="nom" className={s.ojlabel} >Nom :</label>
+        <div class="flex flex-wrap mx-20 space-y-1 md:space-y-5 ">
+          <label htmlFor="nom" className={s.ojlabel} >Nom</label>
           <div class="flex flex-shrink w-full space-x-4">
             <input id="nom" type="text" autoComplete="Nom" className={s.ojinput} placeholder="nom" />
-            <input id="prenom" type="text" autoComplete="Prénom" className={s.ojinput} placeholder="prénom" />           
+            <input id="prenom" type="text" autoComplete="Prenom" className={s.ojinput} placeholder="prénom" />           
           </div>
           <div class="w-full">
-            <label htmlFor="email" className={s.ojlabel} >Email :</label>
+            <label htmlFor="email" className={s.ojlabel} >Email <span className={s.ojetoilerouge}>*</span></label>
             <input id="email" type="text" autoComplete="Email" className={s.ojinput} required />
           </div>
           <div class="w-full">
-            <label htmlFor="message" className={s.ojlabel} >Message :</label>
+            <label htmlFor="message" className={s.ojlabel} >Message <span className={s.ojetoilerouge}>*</span></label>
             <textarea id="message" type="text" autoComplete="Votre Message..." className={s.ojinput} required />
           </div>
           <div class="w-full">
-            <label htmlFor="tel" className={s.ojlabel} >Téléphonne :</label>
+            <label htmlFor="tel" >Téléphonne</label>
             <input id="tel" type="text" className={s.ojinput} autoComplete="" />
           </div>
-          <div class="flex flex-shrink w-full space-x-4">
-            
-            <button type="submit" class="h-10 px-5 border text-ojwhite bg-ojblack transition-colors duration-150 rounded-lg focus:shadow-outline hover:bg-ojgreen hover:text-ojblack hover:font-bold ">Envoyer le message</button>         
+          <div class="flex flex-wrap w-full">
+            <div class="flex flex-col flex-none">
+              <div>Vérification <span className={s.ojetoilerouge}>*</span></div>
+              <div class="g-recaptcha" data-sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_PUBLICSITE_KEY}></div>
+            </div>
+            <div class="flex-auto">
+              <button type="submit" className={s.ojbouton} >Envoyer le message</button>         
+            </div>
           </div>
         </div>
         
       </form>
     )
   }
+  
